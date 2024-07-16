9c5b73fbd821d9709a599918c5119f65f79d9cee
Soham Khedkar <sohamunderscore99@gmail.com>
2024-07-16 08:49:45 +0530

    error sol check

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -6,29 +6,30 @@ import { clerkClient } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
   }
-  // Get the headers
+
   const headerPayload = headers();
-  const svix_id = headerPayload.get('svix-id');
-  const svix_timestamp = headerPayload.get('svix-timestamp');
-  const svix_signature = headerPayload.get('svix-signature');
-  // If there are no headers, error out
+  const svix_id = headerPayload.get('svix-id') ?? '';
+  const svix_timestamp = headerPayload.get('svix-timestamp') ?? '';
+  const svix_signature = headerPayload.get('svix-signature') ?? '';
+
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occurred -- no svix headers', {
       status: 400,
     });
   }
-  // Get the body
+
   const payload = await req.json();
   const body = JSON.stringify(payload);
-  // Create a new Svix instance with your secret.
+
   const wh = new Webhook(WEBHOOK_SECRET);
+
   let evt: WebhookEvent;
-  // Verify the payload with the headers
+
   try {
     evt = wh.verify(body, {
       'svix-id': svix_id,
@@ -41,21 +42,23 @@ export async function POST(req: Request) {
       status: 400,
     });
   }
-  // Get the ID and type
-  const { id } = evt.data;
+
   const eventType = evt.type;
+
   if (eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+
     const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name ?? '',
-      lastName: last_name ?? '',
-      photo: image_url,
+      clerkId: id ?? '',
+      email: email_addresses[0]?.email_address ?? '',
+      username: username ?? '', // Default to empty string if null
+      firstName: first_name ?? '', // Default to empty string if null
+      lastName: last_name ?? '', // Default to empty string if null
+      photo: image_url ?? '',
     };
 
     const newUser = await createUser(user);
+
     if (newUser) {
       await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
@@ -63,23 +66,32 @@ export async function POST(req: Request) {
         },
       });
     }
+
     return NextResponse.json({ message: 'OK', user: newUser });
   }
+
   if (eventType === 'user.updated') {
     const { id, image_url, first_name, last_name, username } = evt.data;
+
     const user = {
-      firstName: first_name ?? '',
-      lastName: last_name ?? '',
-      username: username!,
-      photo: image_url,
+      firstName: first_name ?? '', // Default to empty string if null
+      lastName: last_name ?? '', // Default to empty string if null
+      username: username ?? '', // Default to empty string if null
+      photo: image_url ?? '',
     };
-    const updatedUser = await updateUser(id, user);
+
+    const updatedUser = await updateUser(id ?? '', user);
+
     return NextResponse.json({ message: 'OK', user: updatedUser });
   }
+
   if (eventType === 'user.deleted') {
     const { id } = evt.data;
-    const deletedUser = await deleteUser(id!);
+
+    const deletedUser = await deleteUser(id ?? '');
+
     return NextResponse.json({ message: 'OK', user: deletedUser });
   }
+
   return new Response('', { status: 200 });
 }
