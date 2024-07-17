f466f5f6c9e0aa4b45dddd88f035fc33aa86d286
Soham Khedkar <sohamunderscore99@gmail.com>
2024-07-17 09:44:05 +0530

    EventForm added

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,9 +3,9 @@
 import { revalidatePath } from 'next/cache'
 
 import { connectToDatabase } from '@/lib/database'
-import User from '@/lib/database/models/user.models'
-import Order from '@/lib/database/models/order.models'
-import Event from '@/lib/database/models/event.models'
+import User from '@/lib/database/models/user.model'
+import Order from '@/lib/database/models/order.model'
+import Event from '@/lib/database/models/event.model'
 import { handleError } from '@/lib/utils'
 
 import { CreateUserParams, UpdateUserParams } from '@/types'
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,17 +1,20 @@
-import mongoose from 'mongoose'
-const MONGODB_URI = process.env.MONGODB_URI 
+import mongoose from 'mongoose';
 
-let cached = (global as any).mongoose || {conn:null, promise:null};
+const MONGODB_URI = process.env.MONGODB_URI;
 
-export const connectToDatabase = async()=> {
-    if(cached.conn) return cached.conn;
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI,{
-        dbName:'iVentto',
-        bufferCommands: false,
-    })
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
 
-    cached.conn = await cached.promise;
-    return cached.conn;
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'iVentto',
+    bufferCommands: false,
+  })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
