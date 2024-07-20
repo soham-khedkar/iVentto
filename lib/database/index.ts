351e4e1d01e5a6a2669e9d1c11d1523dcdf64c3a
Soham Khedkar <sohamunderscore99@gmail.com>
2024-07-20 17:15:55 +0530

    fix

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,7 +1,5 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
-dotenv.config();
 const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
@@ -9,14 +7,19 @@ let cached = (global as any).mongoose || { conn: null, promise: null };
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'iVentto',
-    bufferCommands: false,
-  })
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "iVentto",
+      bufferCommands: false,
+    });
 
   cached.conn = await cached.promise;
 
+  if (cached.conn) {
+    console.log("Connected!");
+  }
   return cached.conn;
-}
+};
