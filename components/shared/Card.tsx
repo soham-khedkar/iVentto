8b6368856267a7872398aa66a72e2b06a940364d
Soham Khedkar <sohamunderscore99@gmail.com>
2024-07-25 23:33:32 +0530

    stripe issue

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -63,12 +63,12 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
             {event.organizer?.firstName} {event.organizer?.lastName}
           </p>
 
-          {hasOrderLink && (
+          {/* {hasOrderLink && (
             <Link href={`/orders?eventId=${event._id}`} className="flex gap-2">
               <p className="text-primary-500">Order Details</p>
               <Image src="/assets/icons/arrow.svg" alt="search" width={10} height={10} />
             </Link>
-          )}
+          )} */}
         </div>
       </div>
     </div>
