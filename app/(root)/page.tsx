235bf424d0844492fd4b88d40718285aa469344a
Soham Khedkar <sohamunderscore99@gmail.com>
2024-07-25 23:31:58 +0530

    search and category filter added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,19 @@
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import Image from "next/image"
+import { SearchParamProps } from '@/types';
 import Collection from "@/components/shared/Collection"
 import { getAllEvents } from "@/lib/actions/event.actions"
-export default async function Home(){
+import CategoryFilter from "@/components/shared/CategoryFilter"
+import Search from "@/components/shared/Search";
+export default async function Home({ searchParams }: SearchParamProps){
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
   const events = await getAllEvents({
-    query: '',
-    category: '',
-    page:1,
+    query: searchText,
+    category,
+    page,
     limit: 6
   })
     return(
@@ -36,8 +42,8 @@ export default async function Home(){
       <section id='events' className='wrapper my-8 flex flex-col gap-8 md:gap-12'>
         <h2 className="h2-bold">Trusted by <br/>Thousands of Events.</h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search
-          CategoryFilter
+          <Search/>
+          <CategoryFilter/>
         </div>
         <Collection
         data={events?.data}
